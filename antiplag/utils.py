fcf69150453d2a41333249ccfcc02e9a1f19bca4
Denisa Mensatorisová <32281312+DeMensa123@users.noreply.github.com>
2021-03-31 16:31:26 +0200

    Remove empty lines and replace variable names

--- antiplag/utils.py
+++ antiplag/utils.py
@@ -12,43 +12,40 @@ def merge_intervals(indices):
     stack.append(sorted_indices[0])
     sorted_indices.pop(0)
 
-    for x in sorted_indices:
-
+    for index in sorted_indices:
         top = stack[len(stack) - 1]
 
-        if x["id"] != top["id"]:
-            top = x
+        if index["id"] != top["id"]:
+            top = index
             stack.append(top)
-
-        if top["to"] < x["from"]:
-            stack.append(x)
-
-        elif top["to"] <= x["to"]:
-            top["to"] = x["to"]
+        if top["to"] < index["from"]:
+            stack.append(index)
+        elif top["to"] <= index["to"]:
+            top["to"] = index["to"]
             stack.pop()
             stack.append(top)
 
     starts = []
     ends = []
 
-    for x in stack:
+    for item in stack:
         starts.append(
             {
-                "intervals": [x["from"], 1],
+                "intervals": [item["from"], 1],
                 "doc": {
-                    "id": x["id"],
-                    "name": x["name"],
-                    "percentage": x["percentage"],
+                    "id": item["id"],
+                    "name": item["name"],
+                    "percentage": item["percentage"],
                 },
             }
         )
         ends.append(
             {
-                "intervals": [x["to"] + 1, -1],
+                "intervals": [item["to"] + 1, -1],
                 "doc": {
-                    "id": x["id"],
-                    "name": x["name"],
-                    "percentage": x["percentage"],
+                    "id": item["id"],
+                    "name": item["name"],
+                    "percentage": item["percentage"],
                 },
             }
         )
@@ -63,7 +60,6 @@ def merge_intervals(indices):
         if starts[i]["intervals"][0] < ends[j]["intervals"][0]:
             sorted_positions.append(starts[i])
             i += 1
-
         else:
             sorted_positions.append(ends[j])
             j += 1
@@ -76,22 +72,21 @@ def merge_intervals(indices):
     prev = -1
     count = 0
 
-    for x in sorted_positions:
-        if x["intervals"][0] > prev and count != 0:
+    for item in sorted_positions:
+        if item["intervals"][0] > prev and count != 0:
             intervals.append(
                 {
-                    "ranges": {"from": prev, "to": x["intervals"][0] - 1},
+                    "ranges": {"from": prev, "to": item["intervals"][0] - 1},
                     "matches": docs[:],
                 }
             )
 
-        prev = x["intervals"][0]
-        count += x["intervals"][1]
+        prev = item["intervals"][0]
+        count += item["intervals"][1]
 
-        if x["intervals"][1] == 1:
-            docs.append(x["doc"])
-
+        if item["intervals"][1] == 1:
+            docs.append(item["doc"])
         else:
-            del docs[docs.index(x["doc"])]
+            del docs[docs.index(item["doc"])]
 
     return intervals
