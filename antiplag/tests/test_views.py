3e37de5f6e4efdd508fbb25d4a2160a892dcb0bc
KToldy <kristian.toldy@gmail.com>
2021-04-12 23:04:10 +0200

    BE-73 Fix tests for uuids

--- antiplag/tests/test_views.py
+++ antiplag/tests/test_views.py
@@ -31,7 +31,7 @@ class GraphViewTestCase(TestCase):
             document=self.document2,
             matched_docs=[
                 {"name": "test_name", "elastic_id": 123, "percentage": 0.19},
-                {"name": "test_name", "id": result1.id, "percentage": 0.19},
+                {"name": "test_name", "id": str(self.document.id), "percentage": 0.19},
             ],
         )
 
@@ -39,16 +39,20 @@ class GraphViewTestCase(TestCase):
         result = response.data
         expected = {
             "nodes": [
-                {"id": result1.id, "name": "whatever1", "uploaded": True},
+                {"id": str(self.document.id), "name": "whatever1", "uploaded": True},
                 {"name": "test_name", "id": 123},
                 {"name": "test_name2", "id": 1234},
-                {"id": result2.id, "name": "whatever2", "uploaded": True},
+                {"id": str(self.document2.id), "name": "whatever2", "uploaded": True},
             ],
             "links": [
-                {"source": result1.id, "target": 123, "value": 0.19},
-                {"source": result1.id, "target": 1234, "value": 0.19},
-                {"source": result2.id, "target": 123, "value": 0.19},
-                {"source": result2.id, "target": result1.id, "value": 0.19},
+                {"source": str(self.document.id), "target": 123, "value": 0.19},
+                {"source": str(self.document.id), "target": 1234, "value": 0.19},
+                {"source": str(self.document2.id), "target": 123, "value": 0.19},
+                {
+                    "source": str(self.document2.id),
+                    "target": str(self.document.id),
+                    "value": 0.19,
+                },
             ],
         }
         self.assertDictEqual(expected, result)
@@ -73,10 +77,10 @@ class GraphViewTestCase(TestCase):
         result = response.data
         expected = {
             "nodes": [
-                {"id": result1.id, "name": "whatever1", "uploaded": True},
+                {"id": str(self.document.id), "name": "whatever1", "uploaded": True},
                 {"name": "test_name", "id": 123},
-                {"id": result2.id, "name": "whatever2", "uploaded": True},
+                {"id": str(self.document2.id), "name": "whatever2", "uploaded": True},
             ],
-            "links": [{"source": result1.id, "target": 123, "value": 0.19}],
+            "links": [{"source": str(self.document.id), "target": 123, "value": 0.19}],
         }
         self.assertDictEqual(expected, result)
